a16c7d92da843f332a1c31e16a20e9afa5f13154
Josue Palomo <josuepalomocs@gmail.com>
2023-03-27 16:48:15 -0500

    add divider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import AppWrapper from "./components/Layout/AppWrapper";
 import Calendar from "./components/Feature/Calendar/Calendar";
+import Divider from "./components/Layout/Divider";
 
 function App() {
   return (
     <AppWrapper>
       <Calendar />
+      <Divider />
     </AppWrapper>
   );
 }
---
+++ src/components/Layout/Divider.tsx
@@ -0,0 +1,3 @@
+export default function Divider() {
+  return <hr className="border-neutral-800 my-4" />;
+}
